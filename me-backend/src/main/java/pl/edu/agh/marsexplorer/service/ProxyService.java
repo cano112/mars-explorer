f6aa08a04188bebdbc51ad825d21ee6ffb71c931
Dominik Hendzel <doodzio@hotmail.com>
2018-06-15 01:27:41 +0200

    Revert "Create proxy for NASA API"
    This reverts commit 5e63035de6eceb28fe475f20e5c068d3c3d85a41.

--- me-backend/src/main/java/pl/edu/agh/marsexplorer/service/ProxyService.java
+++
@@ -1,7 +0,0 @@
-package pl.edu.agh.marsexplorer.service;
-
-import org.springframework.http.HttpMethod;
-
-public interface ProxyService {
-    String redirectRequest(String requestBody, HttpMethod httpMethod, String resourceString, String queryString);
-}

7e4debeeed15760b79482afcf2c512e5d303c2ce
Dominik Hendzel <doodzio@hotmail.com>
2018-06-15 01:55:59 +0200

    fixed

--- me-frontend/src/utils/nasaMarsApiWrapper.js
+++ me-frontend/src/utils/nasaMarsApiWrapper.js
@@ -7,24 +7,15 @@ import axios from 'axios';
  * - ROVERS https://api.nasa.gov/mars-photos/api/v1/rovers/?api_key=d4AMs5679WgbHT8C38EoYbCI8ssPsWEdGDRzVgEi
  * - PHOTOS for sol = 0 https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?api_key=d4AMs5679WgbHT8C38EoYbCI8ssPsWEdGDRzVgEi&sol=0
  */
-<<<<<<< HEAD:me-frontend/src/utils/wrapperNASAMarsAPI.js
 
-export class WrapperNASAMarsAPI {
 
-  /**
-   * @return Rover []
-   */
-  static getAllCuriosityImages() {
 
-    return getRovers();
-=======
 export class NasaMarsApiWrapper {
   static getAllCuriosityImages() {
     /**
      * @return Rover []
      */
     NasaMarsApiWrapper.getRovers();
->>>>>>> 0d49b57dd90a128b98bc0836deb1db4191efeaf8:me-frontend/src/utils/nasaMarsApiWrapper.js
     return ["aaa", "bbbb"];
   }
 
